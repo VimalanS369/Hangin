c7c84ef6d1ae7e8e42ca467b230c4c3abffe89db
Vimalan S <vimalansenthil@gmail.com>
2023-06-27 19:41:31 +0530

    Finished User pages and created separate component for activities, Feed

--- hangin/base/views.py
+++ hangin/base/views.py
@@ -52,7 +52,10 @@ def home(request):
     topics = Topic.objects.all()
 
     room_count = rooms.count()
-    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
+
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request,'base/home.html', context)
 
 def logoutUser(request):
@@ -93,6 +96,16 @@ def room(request,pk):
     context = {'room':room, 'room_messages':room_messages, 'participants' : participants}
     return render(request,'base/room.html',context)
 
+
+@login_required(login_url ='login')
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context ={'user':user,'rooms':rooms,'room_messages':room_messages, 'topics':topics}
+    return render(request,'base/profile.html',context)
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
@@ -100,7 +113,10 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
         
     context = {'form':form}
@@ -148,5 +164,5 @@ def deleteMessage(request,pk):
         return HttpResponse("You are not allowed Here!!")
     if request.method == 'POST':
         message.delete()
-        return redirect('')
+        return redirect('home')
     return render(request, 'base/delete.html', {'obj':message})
